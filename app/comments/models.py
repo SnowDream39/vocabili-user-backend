3572997cc923fbef0cb721d344787e9009461229
SnowDream39 <759723417@qq.com>
2025-06-25 00:38:27 +0800

    弃用alembic

--- app/comments/models.py
+++ app/comments/models.py
@@ -6,7 +6,7 @@ from app.users.models import User  # 导入用户模型
 class Comment(Base):
     __tablename__ = "comment"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    article_id: Mapped[str] = mapped_column(String, index=True, default="")
+    article_id: Mapped[str] = mapped_column(String, index=True, server_default="")
     content: Mapped[str] = mapped_column(Text, nullable=False)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"))
     parent_id: Mapped[int] = mapped_column(Integer, ForeignKey('comment.id'), nullable=True)
